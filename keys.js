a4e573483fb0770f6fef4afbd1cf51e18b0d47d1
markliebs <markliebs@me.com>
2016-06-27 18:56:53 -0400

    removed keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('Loaded!');
-
-exports.twitterKeys = {
-  consumer_key: 'gGjHeeEMsf4SkZc5wslwATz5A',
-  consumer_secret: 'UIlSeS8RV9SOMffYU7ksOtonmzCiHVdiWorZQPGChss0tsxKr3',
-  access_token_key: '34808256-HDGGxyxEOjMqvfyj6JyNRvScrUozFJRQysa6jZPuY',
-  access_token_secret: 'a0AE6cNKzcQl3dkM3NClhyyvQPs08PSi1Hdi0PK5cINDP',
-}
